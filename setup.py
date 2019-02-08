0967d958872ae74b3929b722224435dbc4643020
Alex St <al3xst@users.noreply.github.com>
2019-02-08 03:25:25 +0100

    Increase version number in setup.py
    This version is compatible to v.0.1. Only a new function has been introduced.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="pyrnvapi",
-      version="0.1",
+      version="0.2",
       description="Client API for the official REST API provied by the RNV GmbH",
       url="http://github.com/myrnvplan/pyrnvapi",
       author = "Alexander Stepanov",
